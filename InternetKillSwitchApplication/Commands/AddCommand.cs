35af239d098a39ca8d48ce0ad7e044061ef930c3
David Kucsai <jenrel62@gmail.com>
2015-03-22 08:41:37 +0800

    Remove the drag-drop and replace with buttons. Still in progress.

---
+++ InternetKillSwitchApplication/Commands/AddCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace InternetKillSwitchApplication.Commands
+{
+    public class AddCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
